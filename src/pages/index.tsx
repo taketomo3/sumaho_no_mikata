7fa28cd53ee34b21b6d74f468d2bc9ef1b3a003a
竹田智哉 <andalso316@gmail.com>
2021-07-06 19:50:42 +0900

    Make animation of log title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import type { NextPage } from "next";
-import { Layout } from "src/components/layout";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  const handleClick = () => {
-    window.alert("Hello, World!");
-  };
-
   return (
-    <Layout>
-      <button className="p-2" onClick={handleClick}>
-        Click me!
-      </button>
-    </Layout>
+    <div className="h-full min-h-screen mx-2">
+      <span className="block h-4" />
+      <div className="h-screen text-center animate-fadeInMoveTop">
+        <Link href="/">
+          <a>
+            <Image src="/title.svg" alt="すまほのみかた" width={200} height={50} />
+          </a>
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{ts,tsx}"],
   darkMode: false, // 'media' or 'class'
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      keyframes: {
+        fadeInMoveTop: {
+          "0%": { opacity: 0, transform: "translateY(45%)" },
+          "30%": { opacity: 0.5, transform: "translateY(45%)" },
+          "60%": { opacity: 1, transform: "translateY(45%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeInMoveTop: "fadeInMoveTop 4s ease-in-out 1",
+      },
+    },
+  },
   variants: { extend: {} },
   plugins: [],
 };
